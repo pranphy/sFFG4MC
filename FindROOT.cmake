a8dcc2b97936a679bed8839efe776c138df0fcdb
Prakash Gautam <pranphy@gmail.com>
2025-06-10 13:51:04 -0400

    Fix CMake for newer root and cmake version

--- FindROOT.cmake
+++ FindROOT.cmake
@@ -67,13 +67,13 @@ IF (ROOT_CONFIG_EXECUTABLE)
   ENDIF (found_root_major_vers LESS 5)
 
   # compute an overall version number which can be compared at once
-  MATH(EXPR req_vers "${req_root_major_vers}*10000 + ${req_root_minor_vers}*100 + ${req_root_patch_vers}")
-  MATH(EXPR found_vers "${found_root_major_vers}*10000 + ${found_root_minor_vers}*100 + ${found_root_patch_vers}")
+  #MATH(EXPR req_vers "${req_root_major_vers}*10000 + ${req_root_minor_vers}*100 + ${req_root_patch_vers}")
+  #MATH(EXPR found_vers "${found_root_major_vers}*10000 + ${found_root_minor_vers}*100 + ${found_root_patch_vers}")
    
-  IF (found_vers LESS req_vers)
+  IF (found_vers LESS 6.0)
     SET(ROOT_FOUND FALSE)
     SET(ROOT_INSTALLED_VERSION_TOO_OLD TRUE)
-  ELSE (found_vers LESS req_vers)
+  ELSE (found_vers LESS 6.0)
     SET(ROOT_FOUND TRUE)
   ENDIF (found_vers LESS req_vers)
 
